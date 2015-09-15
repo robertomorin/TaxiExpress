1d377122110a98d0f8669646b0edea0a4ee03e93
josecarlossalmong <jnoastey@yahoo.com>
2015-09-15 03:46:29 +0000

    Creacion de tabla Contacts

--- Development/taxiespress/db/schema.rb
+++ Development/taxiespress/db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915025259) do
+ActiveRecord::Schema.define(version: 20150915034151) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.string   "name",       limit: 255
+    t.string   "lastname",   limit: 255
+    t.string   "email",      limit: 255
+    t.string   "adress",     limit: 255
+    t.string   "message",    limit: 255
+    t.integer  "phone",      limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",       limit: 255
