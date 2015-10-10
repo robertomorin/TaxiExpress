60d497dd5f5595bc0fe051b9468e360af6f4e51f
rios1993 <rios1993@outlook.com>
2015-10-10 09:37:20 +0000

    agregando referencia de vehiculos etc

--- Development/taxiexpress/app/models/reservation.rb
+++ Development/taxiexpress/app/models/reservation.rb
@@ -4,4 +4,25 @@ class Reservation < ActiveRecord::Base
   belongs_to :operator
   belongs_to :status
   belongs_to :paymenttype
+  
+  def vehicle_name
+    self.vehicle.name
+  end
+  
+  def operator_name
+    self.operator.name
+  end
+  
+  def user_name
+    self.user.name
+  end
+  
+  def status_name
+    self.status.name
+  end
+  
+  def paymenttype_name
+    self.paymenttype.name
+  end
+  
 end
